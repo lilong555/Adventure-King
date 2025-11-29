fa6b131c204365868345960d8f4635b3bd065b44
lilong <18577171104@163.com>
2025-11-29 19:54:08 +0800

    添加主菜单基础框架与按钮，在 HelloWorld 场景中初始化主菜单，并添加了背景图片。开始游戏 退出游戏 两个菜单项。- 为菜单项绑定了相应的回调函数，实现了基本的 交互逻辑 -移除了默认的资源文件，并引入了新的菜单相关美术资源。

--- Adventure-King/Classes/HelloWorldScene.cpp
+++ Adventure-King/Classes/HelloWorldScene.cpp
@@ -1,26 +1,3 @@
-/****************************************************************************
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
- http://www.cocos2d-x.org
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
 
 #include "HelloWorldScene.h"
 #include "SimpleAudioEngine.h"
@@ -32,14 +9,14 @@ Scene* HelloWorld::createScene()
     return HelloWorld::create();
 }
 
-// Print useful error message instead of segfaulting when files are not there.
+// 当文件不存在时，打印有用的错误消息而不是段错误。
 static void problemLoading(const char* filename)
 {
     printf("Error while loading: %s\n", filename);
     printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
 }
 
-// on "init" you need to initialize your instance
+// 在 "init" 上你需要初始化你的实例
 bool HelloWorld::init()
 {
     //////////////////////////////
@@ -58,15 +35,15 @@ bool HelloWorld::init()
 
     // add a "close" icon to exit the progress. it's an autorelease object
     auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
+                                            "Scene/Menu/CloseNormal.png",
+                                           "Scene/Menu/CloseSelected.png",
                                            CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
 
     if (closeItem == nullptr ||
         closeItem->getContentSize().width <= 0 ||
         closeItem->getContentSize().height <= 0)
     {
-        problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
+        problemLoading("Scene/Menu/CloseNormal.png and Scene/Menu/CloseSelected.png");
     }
     else
     {
@@ -74,38 +51,91 @@ bool HelloWorld::init()
         float y = origin.y + closeItem->getContentSize().height/2;
         closeItem->setPosition(Vec2(x,y));
     }
+    auto StartItem = MenuItemImage::create(
+        "Scene/Menu/StartItemNormal.png",
+        "Scene/Menu/StartItemSelect.png",
+        CC_CALLBACK_1(HelloWorld::menuStartCallback, this));
+    float StartItem_Y = 0,StartItem_X=0;
+    if (StartItem == nullptr ||
+        StartItem->getContentSize().width <= 0 ||
+        StartItem->getContentSize().height <= 0)
+    {
+        problemLoading("Scene/Menu/CloseNormal.png and Scene/Menu/CloseSelected.png");
+    }
+    else
+    {
+        float x = origin.x + visibleSize.width/2;
+        float y = origin.y + visibleSize.height/2 - closeItem->getContentSize().height / 2;
+        StartItem_Y = y;
+        StartItem_X = x;
+        StartItem->setPosition(Vec2(x, y));
+    }
+    auto SetItem = MenuItemImage::create(
+        "Scene/Menu/SetingNormal.png",
+        "Scene/Menu/SetingSelect.png",
+        CC_CALLBACK_1(HelloWorld::menuStartCallback, this));
 
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
+    if (SetItem == nullptr ||
+        SetItem->getContentSize().width <= 0 ||
+        SetItem->getContentSize().height <= 0)
+    {
+        problemLoading("SetingNormal.png and SetingSelec");
+    }
+    else
+    {
+        float x = StartItem_X - SetItem->getContentSize().width/2  - StartItem->getContentSize().width / 2;
+        float y = StartItem_Y - SetItem->getContentSize().height/2- StartItem->getContentSize().height / 2;
+        SetItem->setPosition(Vec2(x, y));
+    }
+    auto MapItem = MenuItemImage::create(
+        "Scene/Menu/MapNormal.png",
+        "Scene/Menu/MapSelect.png",
+        CC_CALLBACK_1(HelloWorld::menuStartCallback, this));
+
+    if (MapItem == nullptr ||
+        MapItem->getContentSize().width <= 0 ||
+        MapItem->getContentSize().height <= 0)
+    {
+        problemLoading("MapNormal.png");
+    }
+    else
+    {
+        float x = StartItem_X ;
+        float y = StartItem_Y - MapItem->getContentSize().height / 2 - StartItem->getContentSize().height / 2;
+        MapItem->setPosition(Vec2(x, y));
+    }
+    auto SaveItem = MenuItemImage::create(
+        "Scene/Menu/SaveNormal.png",
+        "Scene/Menu/SaveSelect.png",
+        CC_CALLBACK_1(HelloWorld::menuStartCallback, this));
+    //SaveItem;
+    if (SaveItem == nullptr ||
+        SaveItem->getContentSize().width <= 0 ||
+        SaveItem->getContentSize().height <= 0)
+    {
+        problemLoading("MapNormal.png");
+    }
+    else
+    {
+        float x = StartItem_X + SaveItem->getContentSize().width / 2 + StartItem->getContentSize().width / 2;
+        float y = StartItem_Y - SaveItem->getContentSize().height / 2 - StartItem->getContentSize().height / 2;
+        SaveItem->setPosition(Vec2(x, y));
+    }
+    //  创建菜单，它是一个自动释放对象
+    auto menu = Menu::create(closeItem, StartItem,SetItem,SaveItem,MapItem, NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
 
     /////////////////////////////
     // 3. add your codes below...
 
-    // add a label shows "Hello World"
-    // create and initialize a label
 
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    if (label == nullptr)
-    {
-        problemLoading("'fonts/Marker Felt.ttf'");
-    }
-    else
-    {
-        // position the label on the center of the screen
-        label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height - label->getContentSize().height));
-
-        // add the label as a child to this layer
-        this->addChild(label, 1);
-    }
 
     // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
+    auto sprite = Sprite::create("Scene/Menu/menuBacground_1.png");
     if (sprite == nullptr)
     {
-        problemLoading("'HelloWorld.png'");
+        problemLoading("'Scene/Menu/menuBacground_1.png'");
     }
     else
     {
@@ -115,19 +145,40 @@ bool HelloWorld::init()
         // add the sprite as a child to this layer
         this->addChild(sprite, 0);
     }
+
     return true;
 }
 
 
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
-    //Close the cocos2d-x game scene and quit the application
+   // 关闭cocos2d - x游戏场景并退出应用程序
     Director::getInstance()->end();
 
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() as given above,instead trigger a custom event created in RootViewController.mm as below*/
+    /*若要在不退出应用的情况下返回到原生 iOS 屏幕（如果存在），请不要使用上面给出的 Director::getInstance()->end()，而是触发在 RootViewController.mm 中创建的自定义事件，如下所示*/
 
     //EventCustom customEndEvent("game_scene_close_event");
     //_eventDispatcher->dispatchEvent(&customEndEvent);
+
+
+}
+void HelloWorld::menuStartCallback(Ref* pSender)
+{
+   
+
+}
+void HelloWorld::menuSaveCallback(Ref* pSender)
+{
+   
+
+}
+void HelloWorld::menuMapCallback(Ref* pSender)
+{
+
+
+}
+void HelloWorld::menuSetCallback(Ref* pSender)
+{
 
 
 }
